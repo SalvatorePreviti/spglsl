dc74dae07de0b8416dbc0d4a718abcedf07531a8
Salvatore Previti <salvatorepreviti@improbable.io>
2021-08-15 12:21:49 +0100

    fix rollup/vite plugin

--- project/packages/spglsl/src/rollup-plugin-spglsl.ts
+++ project/packages/spglsl/src/rollup-plugin-spglsl.ts
@@ -7,27 +7,45 @@ import {
 } from './spglsl-compile'
 
 export interface RollupPluginSpglslOptions extends SpglslAngleCompileOptions {
+  isProduction?: boolean
   extensions?: string[]
 
   onSpglslDone?: (spglslResult: SpglslAngleCompileResult) => void | string | Promise<void> | Promise<string>
 }
 
-const defaultOptions: RollupPluginSpglslOptions = {
-  extensions: ['.frag', '.vert', '.comp']
-}
-
 /**
- * A plugin for vite
+ * A plugin for rollup and vite
  */
 export function rollupPluginSpglsl(options: RollupPluginSpglslOptions) {
-  const finalOptions = { ...defaultOptions }
+  const finalOptions = { ...options }
   if (finalOptions.minify === undefined && options.compileMode === 'Optimize') {
     finalOptions.minify = true
   }
 
-  const extensionsSet = new Set(finalOptions.extensions)
+  const extensionsSet = new Set(finalOptions.extensions || rollupPluginSpglsl.defaultExtensions)
+
+  function finalizeSettings(isProduction: boolean) {
+    finalOptions.isProduction = !!isProduction
+    if (finalOptions.compileMode === undefined) {
+      finalOptions.compileMode = isProduction ? 'Optimize' : 'Validate'
+    }
+    if (finalOptions.minify === undefined) {
+      finalOptions.minify = isProduction && finalOptions.compileMode === 'Optimize'
+    }
+  }
+
+  // This hook get called with the final resolved vite config.
+  const configResolved = ({ isProduction }: { isProduction?: boolean }) => {
+    if (isProduction !== undefined && finalOptions.isProduction === undefined) {
+      finalizeSettings(isProduction)
+    }
+  }
 
   async function spglslTransform(this: any, code: string, id: string) {
+    if (finalOptions.isProduction === undefined) {
+      finalizeSettings(process.env.NODE_ENV === 'production')
+    }
+
     if (!extensionsSet.has(path.posix.extname(id))) {
       return undefined
     }
@@ -51,7 +69,10 @@ export function rollupPluginSpglsl(options: RollupPluginSpglslOptions) {
   }
 
   return {
-    name: 'vite-plugin-spglsl',
+    name: 'spglsl',
+    configResolved,
     transform: spglslTransform
   }
 }
+
+rollupPluginSpglsl.defaultExtensions = ['.frag', '.vert', '.comp']
